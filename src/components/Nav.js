d4e45bcc8e221950e0ac344f27bca7b81fdbd3ef
TatsuKenshi <rockinbg@gmail.com>
2022-01-31 17:06:31 +0100

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,38 +1,133 @@
 import StyledNav from "../style/StyledNav";
+import { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import img from "../img/logo.png"
+import img from "../img/logo.png";
 
-const Nav = ({user, setUser, purchase}) => {
+const Nav = ({ user, setUser, purchase }) => {
   const history = useHistory();
+
+  // useState for menu toggle on small screens
+  const [showSmall, setShowSmall] = useState(false);
 
   return (
     <StyledNav>
-        
-      <div>
-        <Link to="/"><img src={img} alt="" width="60px" height="60px" /></Link>
+      <div className="nav">
+        <div>
+          <Link to="/">
+            <img src={img} alt="" width="60px" height="60px" />
+          </Link>
+        </div>
+        <div className="link">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="link">
+          <Link to="/games">Search Games</Link>
+        </div>
+        <div className="link">
+          <Link to="/gamenews">Game News</Link>
+        </div>
+        <div className="link">
+          <Link to="/giveaways">Giveaways</Link>
+        </div>
+        <div className="link">
+          <Link to="/merch">Merch</Link>
+        </div>
+        {user && user.userCategory !== "admin" ? (
+          <div className="link">
+            <Link to="/cart">
+              Cart {purchase.length > 0 ? purchase.length : ""}
+            </Link>
+          </div>
+        ) : (
+          ""
+        )}
+        {user ? (
+          <button
+            className="logOutBtn link"
+            onClick={() => {
+              setUser(null);
+              history.push("/");
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <div className="link">
+            <Link to="/login">Login</Link>
+          </div>
+        )}
+        {user ? (
+          ""
+        ) : (
+          <div className="link">
+            <Link to="/register">Register</Link>
+          </div>
+        )}
+        {user ? <div className="link">{user?.username}</div> : ""}
+
+        <div
+          className="burger"
+          onClick={() => {
+            setShowSmall(!showSmall);
+          }}
+        >
+          <div class="stripe"></div>
+          <div class="stripe"></div>
+          <div class="stripe"></div>
+        </div>
       </div>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <Link to="/games">Search Games</Link>
-      </div>
-      <div>
-        <Link to="/gamenews">Game News</Link>
-      </div>
-      <div>
-        <Link to="/giveaways">Giveaways</Link>
-      </div>
-      <div>
-        <Link to="/merch">Merch</Link>
-      </div>
-      {user && user.userCategory !=="admin"? <div><Link to="/cart">Cart {purchase.length > 0 ? purchase.length : ""}</Link></div> : ""}
-        {user? <button className="logOutBtn" onClick={()=>{
-          setUser(null);
-          history.push("/");
-        }}>Logout</button> : <div><Link to="/login">Login</Link></div> }
-        {user? "" : <div><Link to="/register">Register</Link></div> }
-        {user? <div>{user?.username}</div> : "" }
+
+      {showSmall && (
+        <div className="small-menu">
+          <div className="link-small">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="link-small">
+            <Link to="/games">Search Games</Link>
+          </div>
+          <div className="link-small">
+            <Link to="/gamenews">Game News</Link>
+          </div>
+          <div className="link-small">
+            <Link to="/giveaways">Giveaways</Link>
+          </div>
+          <div className="link-small">
+            <Link to="/merch">Merch</Link>
+          </div>
+          {user && user.userCategory !== "admin" ? (
+            <div className="link-small">
+              <Link to="/cart">
+                Cart {purchase.length > 0 ? purchase.length : ""}
+              </Link>
+            </div>
+          ) : (
+            ""
+          )}
+          {user ? (
+            <button
+              className="logOutBtn link-small"
+              onClick={() => {
+                setUser(null);
+                history.push("/");
+              }}
+            >
+              Logout
+            </button>
+          ) : (
+            <div className="link-small">
+              <Link to="/login">Login</Link>
+            </div>
+          )}
+          {user ? (
+            ""
+          ) : (
+            <div className="link-small">
+              <Link to="/register">Register</Link>
+            </div>
+          )}
+          {user ? <div className="link-small">{user?.username}</div> : ""}
+        </div>
+      )}
     </StyledNav>
   );
 };
--- src/style/StyledNav.js
+++ src/style/StyledNav.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 const StyledNav = styled.div`
-  display: flex;
-  justify-content: space-around;
-  color: indianred;
-  /* padding: 5px; */
-  text-align: center;
-  align-items: center;
-  font-size: 1.1rem;
-  font-weight:900;
-  background: rgba(0, 255, 255, 0.2);
-  margin-bottom: 20px;
+  .nav {
+    display: flex;
+    width: 100vw;
+    justify-content: space-around;
+    color: indianred;
+    text-align: center;
+    align-items: center;
+    font-size: 1.1rem;
+    font-weight: 900;
+    background: rgba(0, 255, 255, 0.2);
+  }
 
   a {
     text-decoration: none;
@@ -18,7 +19,7 @@ const StyledNav = styled.div`
     font-weight: 900;
   }
 
-  div:hover {
+  .link:hover {
     text-decoration: underline;
   }
 
@@ -35,6 +36,46 @@ const StyledNav = styled.div`
     text-decoration: underline;
   }
 
+  .burger {
+    display: none;
+  }
+
+  .stripe {
+    width: 40px;
+    height: 7px;
+    background: indianred;
+  }
+
+  .small-menu {
+    background: pink;
+    text-align: center;
+    display: none;
+    background: rgba(0, 255, 255, 0.2);
+  }
+
+  .link-small {
+    height: 25px;
+  }
+
+  @media (max-width: 768px) {
+    .link {
+      display: none;
+    }
+
+    .burger {
+      width: 40px;
+      height: 30px;
+      margin-left: auto;
+      margin-right: 5%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .small-menu {
+      display: block;
+    }
+  } ;
 `;
 
 export default StyledNav;
